a233500641f14838ca2b2ef6970571e9434ca072
teffcode <tefy.1@live.com>
2018-06-03 15:17:05 -0500

    Change name to FruitsList

--- src/components/FruitsList.js
+++ src/components/FruitsList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
-const GifsTemp = ({ gifs }) => {
+const FruitsList = ({ gifs }) => {
     const gifItems = gifs.map(gif => <li key={ gif.id }>{ gif.url }</li>);
     return <ul>{ gifItems }</ul>
 }
+
+export default FruitsList;
